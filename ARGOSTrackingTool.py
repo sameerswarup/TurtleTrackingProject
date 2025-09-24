0aae2b9fc3cc460c3a6e8673bb41d49e1d52da44
sameerswarup <sameerswarup77@gmail.com>
2025-09-24 12:54:58 -0400

    Added in code to parse one line of sara.txt data

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -8,3 +8,11 @@
 # Author: Sameer Swarup (sameer.swarup@duke.edu)
 # Date:   Fall 2025
 #--------------------------------------------------------------
+
+#Parse data from sara.txt
+#Create a string variable that points to a copy and pasted line of data from sara.txt
+lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
+
+#Create a list variable that splits lineString based on spaces and points to a list of entries from the line of data
+lineData = lineString.split()
+
