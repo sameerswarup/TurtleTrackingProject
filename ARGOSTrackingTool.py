d00f49151cee66e38eeaa66ece91f3d5ea3f2ec5
sameerswarup <sameerswarup77@gmail.com>
2025-09-24 13:00:33 -0400

    Assigned variables to items in lineData list

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -15,4 +15,11 @@ lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-
 
 #Create a list variable that splits lineString based on spaces and points to a list of entries from the line of data
 lineData = lineString.split()
+print(lineData)
 
+# Assign variables to specfic items in the list
+record_id = lineData[0]  # ARGOS tracking record ID
+obs_date = lineData[2] # Observation date
+ob_lc = lineData[4]      # Observation Location Class
+obs_lat = lineData[6]    # Observation Latitude
+obs_lon = lineData[7]    # Observation Longitude
