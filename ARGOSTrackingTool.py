517df7c51cb95da4a802c62835499f7e3f10dc01
sameerswarup <sameerswarup77@gmail.com>
2025-09-29 12:32:23 -0400

    Revert "Changed for loop to while loop to read fi;e"
    This reverts commit a6f37e574817f3865cb239752f6d200a2a3a5c47.

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -17,19 +17,17 @@ file_object = open(file_name,'r')
 #Full path: "V:\VSCodeTest\TurtleTrackingProject\data\raw\Sara.txt"
 
 #Read contents of file into a list, one line at a time
-lineString = file_object.readline()
+line_list = file_object.readlines()
 
 #Close the file
-#file_object.close()
+file_object.close()
 
 #Parse data from sara.txt
 #Iterate through all data lines in Sara.txt
-#Iterate through lines
-while lineString != "":
+for lineString in line_list:
     #Create a list variable that splits lineString based on spaces and points to a list of entries from the line of data
     lineData = lineString.split()
     if lineData[0] in ['#', 'u']:
-        lineString = file_object.readline()
         continue
     # Assign variables to specfic items in the list
     record_id = lineData[0]  # ARGOS tracking record ID
@@ -37,9 +35,6 @@ while lineString != "":
     ob_lc = lineData[4]      # Observation Location Class
     obs_lat = lineData[6]    # Observation Latitude
     obs_lon = lineData[7]    # Observation Longitude
-
-    lineString = file_object.readline()
-file_object.close()
 
     #Print the location of sara
     #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
