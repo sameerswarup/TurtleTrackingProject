5941f353008d1dd87e03354a3f615fa5d82fbde8
sameerswarup <sameerswarup77@gmail.com>
2025-09-29 12:17:26 -0400

    Added a for loop to parse all data lines in Sara.txt

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,26 +16,25 @@ file_object = open(file_name,'r')
 
 #Full path: "V:\VSCodeTest\TurtleTrackingProject\data\raw\Sara.txt"
 
-#Read contents of file into a list
+#Read contents of file into a list, one line at a time
 line_list = file_object.readlines()
 
 #Close the file
 file_object.close()
 
 #Parse data from sara.txt
-#Create a string variable that points to a copy and pasted line of data from sara.txt
-lineString = line_list[202]
+#Iterate through all data lines in Sara.txt
+for lineString in line_list:
+    #Create a list variable that splits lineString based on spaces and points to a list of entries from the line of data
+    lineData = lineString.split()
+    if lineData[0] in ['#', 'u']:
+        continue
+    # Assign variables to specfic items in the list
+    record_id = lineData[0]  # ARGOS tracking record ID
+    obs_date = lineData[2] # Observation date
+    ob_lc = lineData[4]      # Observation Location Class
+    obs_lat = lineData[6]    # Observation Latitude
+    obs_lon = lineData[7]    # Observation Longitude
 
-#Create a list variable that splits lineString based on spaces and points to a list of entries from the line of data
-lineData = lineString.split()
-print(lineData)
-
-# Assign variables to specfic items in the list
-record_id = lineData[0]  # ARGOS tracking record ID
-obs_date = lineData[2] # Observation date
-ob_lc = lineData[4]      # Observation Location Class
-obs_lat = lineData[6]    # Observation Latitude
-obs_lon = lineData[7]    # Observation Longitude
-
-#Print the location of sara
-print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    #Print the location of sara
+    #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
